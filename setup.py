d6201fa4cdf195b81a9094a73d6ddd0f1adb6589
Ralph Bean <ralph.bean@gmail.com>
2011-03-18 13:38:29 -0400

    classifiers and pypi info and script rename

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0'
+version = '0.1'
 
 setup(name='bitlyclip',
       version=version,
       description="bitly-fy a url in the linux clipboard",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      long_description="",
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.5',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Utilities',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Text Processing :: General',
+      ],
       keywords='',
       author='Ralph Bean',
       author_email='ralph.bean@gmail.com',
@@ -17,11 +28,11 @@ setup(name='bitlyclip',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
+      scripts=['scripts/bitlyclip',],
       install_requires=[
           'bitlyapi',
-          # -*- Extra requirements: -*-
       ],
       entry_points="""
       # -*- Entry points: -*-
       """,
-      )
+     )
