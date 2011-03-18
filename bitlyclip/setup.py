6aba1a081fcf8011debfd8b02c047bc21a1c21b8
Ralph Bean <ralph.bean@gmail.com>
2011-03-18 13:26:16 -0400

    paster create output

---
+++ bitlyclip/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='bitlyclip',
+      version=version,
+      description="bitly-fy a url in the linux clipboard",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Ralph Bean',
+      author_email='ralph.bean@gmail.com',
+      url='http://github.com/ralphbean/bitlyclip',
+      license='GPLv3+',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
