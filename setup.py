0b4d2adc0304e7bba979e238c38404636ce676b5
Ralph Bean <ralph.bean@gmail.com>
2011-03-18 14:22:38 -0400

    added longdesc for pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1.1'
+version = '0.1.2'
 
 setup(name='bitlyclip',
       version=version,
       description="bitly-fy a url in the linux clipboard",
-      long_description="",
+      long_description="""
+      `bitlyclip` is just a script that:
+
+        - takes a url in your clipboard
+        - bitly-ifies it
+        - puts the new url back in your clipboard
+
+      It makes a nice 'hotkey' in whatever window manager you're using.
+
+      If you have notify-python installed, it will
+      pop up nice messages for you.
+
+        - http://www.galago-project.org/downloads.php
+
+      To install:  ``pip install bitlyclip``
+
+      To use: ``$ bitlyclip``
+      """,
       classifiers=[
           'Development Status :: 4 - Beta',
           'Intended Audience :: Developers',
